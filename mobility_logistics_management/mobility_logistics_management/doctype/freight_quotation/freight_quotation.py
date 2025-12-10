fbf28e33724228779516a543400c0a97a958a838
citybirdman <your-email@domain.com>
2025-12-10 11:57:17 +0000

    Initializing Supplier Quotation

--- mobility_logistics_management/mobility_logistics_management/doctype/freight_quotation/freight_quotation.py
+++ mobility_logistics_management/mobility_logistics_management/doctype/freight_quotation/freight_quotation.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, Ahmed Zaytoon and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.model.naming import make_autoname
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 
 class FreightQuotation(Document):
-	pass
+	def autoname(self):
+		if not self.valid_from or not self.quoted_by:
+			frappe.throw("Both (Valid From) and (Quoted By) must be set before saving.")
+		d = getdate(self.valid_from)
+		month = d.month
+		year = d.year
+		prefix = f"{self.quoted_by}-{month}-{year}-"
+		self.name = make_autoname(prefix.upper() + ".##")
--- mobility_logistics_management/mobility_logistics_management/doctype/logistics_management_settings/logistics_management_settings.py
+++ mobility_logistics_management/mobility_logistics_management/doctype/logistics_management_settings/logistics_management_settings.py
@@ -7,6 +7,8 @@ from frappe.model.document import Document
 
 class LogisticsManagementSettings(Document):
 	def validate(self):
+		if not 1 <= self.auto_update_job_trigger <= 24:
+			frappe.throw("Auto Update Job Trigger must be between 1 and 24 hours.")
 		method = "mobility_logistics_management.mobility_logistics_management.shipping_report_data.Update_shipping_report_data"
 
 		if frappe.db.get_value("Scheduled Job Type", {"method": method}):
@@ -17,7 +19,7 @@ class LogisticsManagementSettings(Document):
 				},
 			).update(
 				{
-					"cron_format": f"0/{self.auto_update_job_trigger * 60} * * * *",
+					"cron_format": f"0 0/{self.auto_update_job_trigger} * * *",
 				}
 			).save()
 		else:
@@ -25,7 +27,7 @@ class LogisticsManagementSettings(Document):
 				{
 					"doctype": "Scheduled Job Type",
 					"method": method,
-					"cron_format": f"0/{self.auto_update_job_trigger * 60} * * * *",
+					"cron_format": f"0 0/{self.auto_update_job_trigger} * * *",
 					"create_log": True,
 					"stopped": False,
 					"frequency": "Cron",

db779e40e56780d6062cf3258d2ab12eb40200d9
Ahmed Gamal <ag2121521@gmail.com>
2026-01-27 13:08:24 +0000

    shipping_file

--- mobility_logistics_management/mobility_logistics_management/shipping_report_data.py
+++ mobility_logistics_management/mobility_logistics_management/shipping_report_data.py
@@ -29,11 +29,11 @@ def get_data():
     try: 
         link=frappe.db.sql("SELECT value FROM `tabSingles` WHERE doctype = 'Logistics Management Settings' AND field = 'shipping_report_dropbox_shared_uri_path'", as_dict=True)
         path=link[0]['value']
-        shipping_file=pd.read_excel(f'https://www.dropbox.com/scl/fi/8opxja4ddlygpqm1dniqp/ALMOMTAZ-SHIPPING-REPORT-2026.xlsx?rlkey=zdupycob4e0ykkdc3j3v24fb2&dl=1')
+        shipping_file=pd.read_excel(f'https://www.dropbox.com{path}')
         shipping_file.columns = shipping_file.iloc[shipping_file[shipping_file.columns[1]].dropna().index[0]]
         shipping_file.columns=shipping_file.columns.str.lower()
         shipping_file = shipping_file.iloc[shipping_file[shipping_file.columns[1]].dropna().index[0]+1:].reset_index(drop=True)
-        master_data=pd.read_excel(f'https://www.dropbox.com/scl/fi/8opxja4ddlygpqm1dniqp/ALMOMTAZ-SHIPPING-REPORT-2026.xlsx?rlkey=zdupycob4e0ykkdc3j3v24fb2&dl=1',sheet_name='master_data')
+        master_data=pd.read_excel(f'https://www.dropbox.com{path}',sheet_name='master_data')
         shipping_file.etd=pd.to_datetime(shipping_file.etd,errors='coerce').dt.date
         shipping_file=shipping_file[shipping_file.etd>=pd.to_datetime('2000-01-01').date()]
         shipping_file=shipping_file[~shipping_file.etd.isna()].reset_index(drop=True)        

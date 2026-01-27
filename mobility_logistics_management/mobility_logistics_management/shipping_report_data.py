0d43f8f1ee4ded47ee0cc79b5e894d34344cfb84
Ahmed Gamal <ag2121521@gmail.com>
2026-01-27 13:56:16 +0000

    Update shipping report data retrieval to use a direct link for the shipping report and master data files

--- mobility_logistics_management/mobility_logistics_management/shipping_report_data.py
+++ mobility_logistics_management/mobility_logistics_management/shipping_report_data.py
@@ -6,34 +6,35 @@ from rapidfuzz import fuzz, process
 @frappe.whitelist()
 def get_data():
     columns={
-        'order no.': 'title', # Mandatory
-        'vol.': 'cntr_vol', # 0
-        'type': 'cntr_type', # None
-        'docs':'docs_received', # 0
-        'shipping line': 'liner', # None
-        'forwarder': 'forwarder', # None
-        'pol': 'pol', # None
-        'pod': 'pod', # None
-        'etd': 'shipping_date', # Mandatory
-        'eta': 'arrival_date', # None
-        'ata': 'arrived', # 0
-        'return': 'cntr_returned', # 0
-        'f/t': 'free_time', # 0
-        'bol no.': 'bol_no', # None
-        'freight/cntr': 'freight_per_cntr', # 0
-        'incoterm':'incoterm' # None
+        'order no.': 'title', # Mandatory0
+        'vol.': 'cntr_vol', # 01
+        'type': 'cntr_type', # None02
+        'docs':'docs_received', # 03
+        'shipping line': 'liner', # None04
+        'forwarder': 'forwarder', # None05
+        'pol': 'pol', # None06
+        'pod': 'pod', # None07
+        'etd': 'shipping_date', # Mandatory08
+        'eta': 'arrival_date', # None09
+        'ata': 'arrived', # 010
+        'return': 'cntr_returned', # 011
+        'f/t': 'free_time', # 012
+        'bol no.': 'bol_no', # None 13
+        'freight/cntr': 'freight_per_cntr', # 014
+        'incoterm':'incoterm' # None15
     }
     headers=list(columns.keys())
     new_headers=list(columns.values())
+
     
     try: 
         link=frappe.db.sql("SELECT value FROM `tabSingles` WHERE doctype = 'Logistics Management Settings' AND field = 'shipping_report_dropbox_shared_uri_path'", as_dict=True)
         path=link[0]['value']
-        shipping_file=pd.read_excel(f'https://www.dropbox.com{path}')
+        shipping_file=pd.read_excel(f'https://www.dropbox.com/scl/fi/8opxja4ddlygpqm1dniqp/ALMOMTAZ-SHIPPING-REPORT-2026.xlsx?rlkey=zdupycob4e0ykkdc3j3v24fb2&dl=1')
         shipping_file.columns = shipping_file.iloc[shipping_file[shipping_file.columns[1]].dropna().index[0]]
         shipping_file.columns=shipping_file.columns.str.lower()
         shipping_file = shipping_file.iloc[shipping_file[shipping_file.columns[1]].dropna().index[0]+1:].reset_index(drop=True)
-        master_data=pd.read_excel(f'https://www.dropbox.com{path}',sheet_name='master_data')
+        master_data=pd.read_excel(f'https://www.dropbox.com/scl/fi/8opxja4ddlygpqm1dniqp/ALMOMTAZ-SHIPPING-REPORT-2026.xlsx?rlkey=zdupycob4e0ykkdc3j3v24fb2&dl=1',sheet_name='master_data')
         shipping_file.etd=pd.to_datetime(shipping_file.etd,errors='coerce').dt.date
         shipping_file=shipping_file[shipping_file.etd>=pd.to_datetime('2000-01-01').date()]
         shipping_file=shipping_file[~shipping_file.etd.isna()].reset_index(drop=True)        
@@ -69,11 +70,11 @@ def get_data():
             else:
                 frappe.throw(f"This port <b>{port_name}</b> does not exist in Port of Loading Master. Please add it accordingly.")
         for idx, row in shipping_file.iterrows():
-            shipping_line = row[new_headers[4]] # Shipping Line column
+            shipping_line = row[new_headers[4]].strip().replace(' ','-') # Shipping Line column
             best_match = None
             best_ratio = 0
             for _, row2 in master_data.iterrows():
-                shipping_line2 = row2['liner'].strip().replace(' ','-')
+                shipping_line2 = row2['liner']
                 ratio = fuzz.ratio(shipping_line.lower().split('/')[0], shipping_line2.lower().strip().split('-')[0])
                 if ratio > best_ratio:  
                     best_ratio = ratio
@@ -87,12 +88,12 @@ def get_data():
             else:
                 frappe.throw(f"This shipping line <b>{shipping_line}</b> does not exist in Shipping Line Master. Please add it accordingly.")
         for idx, row in shipping_file.iterrows():
-            forwarder = row[new_headers[5]] # Forwarder column
+            forwarder = row[new_headers[5]].strip().replace(' ','-') # Forwarder column
             best_match = None
             best_ratio = 0
             for _, row2 in master_data.iterrows():
                 forwarder2 = row2['forwarder']
-                ratio = fuzz.ratio(forwarder.lower().strip().split('-')[0], forwarder2.lower().strip().split('-')[0])
+                ratio = fuzz.ratio(forwarder.lower().strip().split('/')[0], forwarder2.lower().strip().split('-')[0])
                 if ratio > best_ratio:  
                     best_ratio = ratio
                     best_match = forwarder2
@@ -140,7 +141,9 @@ def get_data():
         shipping_file[new_headers[12]]=shipping_file[new_headers[12]].replace('',0) 
         shipping_file[new_headers[14]]=shipping_file[new_headers[14]].replace('',0) 
         shipping_file.replace('',None,inplace=True)
-        
+        shipping_file[new_headers[14]]=shipping_file[new_headers[14]].astype(float)
+        shipping_file[new_headers[10]]=shipping_file[new_headers[10]].astype(int)
+
         return shipping_file.to_dict(orient='records')
     
     except Exception as e:
